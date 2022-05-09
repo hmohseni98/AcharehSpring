8fcb816a203b5c9806a048ec0a4b63aaf9835882
hmohseni98 <mohseni.7798@gmail.com>
2022-05-09 19:37:48 +0430

    create spring boot project

---
+++ src/main/java/com/spring/achareh/AcharehApplication.java
@@ -0,0 +1,13 @@
+package com.spring.achareh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcharehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcharehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/achareh/AcharehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.achareh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcharehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
