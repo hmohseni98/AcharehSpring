0794a63cf37a535ada1634d557d8ee64d57eddc5
hmohseni98 <mohseni.7798@gmail.com>
2022-05-14 13:34:29 +0430

    displacement method into user service

--- src/main/java/com/spring/achareh/service/CustomerService.java
+++ src/main/java/com/spring/achareh/service/CustomerService.java
@@ -4,10 +4,5 @@ import com.spring.achareh.model.Customer;
 import com.spring.achareh.service.base.BaseService;
 
 public interface CustomerService extends BaseService<Customer, Integer> {
-//    Customer login(String email, String password);
-//
-//    Customer findByEmail(String email);
-//
-//    void changePassword(Integer id, String oldPassword, String newPassword);
 
 }
