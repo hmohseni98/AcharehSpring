fece53f840d356ea0fbec8a19701c3808c533e5d
hmohseni98 <mohseni.7798@gmail.com>
2022-05-18 09:55:07 +0430

    add field validation using annotation

--- src/main/java/com/spring/achareh/service/dto/expert/ExpertRegisterDTO.java
+++ src/main/java/com/spring/achareh/service/dto/expert/ExpertRegisterDTO.java
@@ -1,24 +1,35 @@
 package com.spring.achareh.service.dto.expert;
 
+import com.spring.achareh.util.customValidation.password.ValidPassword;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class ExpertRegisterDTO {
+
+    @NotBlank
+    @Size(min = 3, max = 30)
     private String firstName;
 
+    @NotBlank
+    @Size(min = 3, max = 50)
     private String lastName;
 
+    @Email
     private String email;
 
+    @ValidPassword
     private String password;
 
     private MultipartFile image;
