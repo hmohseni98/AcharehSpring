826561476362452b6985fdb02092d4c5a1487a10
hmohseni98 <mohseni.7798@gmail.com>
2022-06-04 17:45:45 +0430

    implement user details service class

---
+++ src/main/java/com/spring/achareh/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.spring.achareh.service.impl;
+
+import com.spring.achareh.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Username#" + email + " not found!"));
+    }
+}
